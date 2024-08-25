c0e160ca94d0d9238e9730b0c7a726945413fb48
samuel161415 <samuelnegalign1@gmail.com>
2024-08-25 10:41:22 +0300

    fixing the footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -3,10 +3,10 @@
     class="w-full bg-gray-100 flex flex-col justify-center items-center p-4 text-center mt-8 py-10 "
   >
     <div
-      class="w-full max-w-7xl flex justify-between items-center border-b pb-10"
+      class="w-full max-w-7xl flex flex-col md:flex-row  justify-between items-center border-b pb-10"
     >
-      <h1 class="font-bold">ToNudge</h1>
-      <section class="flex text-primary_txt_color">
+      <h1 class="font-bold max-md:mb-3">ToNudge</h1>
+      <section class="flex justify-between w-full items-center text-primary_txt_color">
         <p class="mx-2 font-[500]">How it Works</p>
         <p class="mx-2 font-[500]">Benefits</p>
         <p class="mx-2 font-[500]">Pricing</p>
@@ -16,7 +16,7 @@
     <div
       class="w-full max-w-7xl mx-auto mt-8 flex justify-between items-center"
     >
-      <p>&copy; 2024 ToNudge. All rights reserved.</p>
+      <p class="text-tertiary_txt_color">&copy; 2024 ToNudge.</p>
       <a href="mailto:contact@tonudge.com" target="_blank" class="flex justify-center items-center">
         <img :src="mail" alt="mail" class="mr-2"/>
         <p class="">Contact: info@tonudge.com</p>
